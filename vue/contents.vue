3aca2b480db757c3cb0f685f540796e875fa501d
Organic Research Centre <124708264+OrgResCent@users.noreply.github.com>
2025-11-06 11:54:49 +0000

    Update contents.vue

--- vue/contents.vue
+++ vue/contents.vue
@@ -10,7 +10,7 @@ module.exports = {
     <h1 class="title text-center">REFOREST Public Goods Tool</h1>
     <p>Welcome to the REFORST version of the Public Goods Tool, a whole-farm sustainability assessment. This tool has been developed by the University of Reading and Organic Research Centre as part of the <a href="https://agroreforest.eu/" target="_blank" rel="noreferrer noopener">European Horizon funded REFOREST project</a>.</p>
     <p>Further details about the PG Tool can be found at the <a href="https://www.organicresearchcentre.com/our-research/research-project-library/public-goods-tool-2/" target="_blank" rel="noreferrer noopener">Organic Research Centre website.</a></p>
-    <p>A user guide for the REFOREST PG Tool is available <a href="https://github.com/organicresearchcentre/pgtool-gui/blob/main/PG%20Tool%20Online%20v3.1%20Manual.md " target="_blank" rel="noreferrer noopener">here.</a></p>
+    <p>A user guide for the REFOREST PG Tool is available <a href="https://github.com/organicresearchcentre/pgtool-reforest-gui/blob/main/PG%20Tool%20Online%20v3.1%20Manual.md " target="_blank" rel="noreferrer noopener">here.</a></p>
     <p>Compulsory questions are marked with a <span class="compulsory">*</span></p>
     <p>Answer fields that are greyed out are automatically filled in.</p>
     <p class="mb-5">Further information can be found by clicking on the <i class="far fa-question-circle fa-xs"></i> next to the questions.</p>
