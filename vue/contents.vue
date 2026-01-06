b67ea594af25d4e21736757c9e110d69aaabb572
cwgossel <108569860+cwgossel@users.noreply.github.com>
2026-01-06 12:28:44 +0000

    Updated EU and UKRI disclaimers

--- vue/contents.vue
+++ vue/contents.vue
@@ -10,10 +10,13 @@ module.exports = {
     <h1 class="title text-center">REFOREST Public Goods Tool</h1>
     <p>Welcome to the REFORST version of the Public Goods Tool, a whole-farm sustainability assessment. This tool has been developed by the University of Reading and Organic Research Centre as part of the <a href="https://agroreforest.eu/" target="_blank" rel="noreferrer noopener">European Horizon funded REFOREST project</a>.</p>
     <p>Further details about the PG Tool can be found at the <a href="https://www.organicresearchcentre.com/our-research/research-project-library/public-goods-tool-2/" target="_blank" rel="noreferrer noopener">Organic Research Centre website.</a></p>
-    <p>A user guide for the REFOREST PG Tool is available <a href="https://github.com/organicresearchcentre/pgtool-reforest-gui/blob/main/REFOREST%20PG%20Tool%20Online%20Manual.md " target="_blank" rel="noreferrer noopener">here.</a></p>
+    <p>A user guide for the REFOREST PG Tool is available <a href="https://github.com/organicresearchcentre/pgtool-gui/blob/main/PG%20Tool%20Online%20v3.1%20Manual.md " target="_blank" rel="noreferrer noopener">here.</a></p>
     <p>Compulsory questions are marked with a <span class="compulsory">*</span></p>
     <p>Answer fields that are greyed out are automatically filled in.</p>
     <p class="mb-5">Further information can be found by clicking on the <i class="far fa-question-circle fa-xs"></i> next to the questions.</p>
-    <img src="img/eulogo.png">
+    <img src="img/eulogo.jpg" class = "lg">
+    <p>This project has recieved funding from the European Union's Horizon Europe research and innovation programme under grant agreement No. 101060635 (REFOREST). Views and opinions expressed are however those of the author(s) only and do not necessarily reflect those of the European Union or the European Research Executive Agency (REA). Neither the European Union nor the granting authority can be held responsible for them.</p>
+    <img src="img/UKRIlogo.webp" class = "lg">
+    <p>UK partners are funded by UK Research and Innovation (UKRI) under UK government's Horizon Europe funding guarantee [grant number 10039700].</p>
   </div>
 </template>
